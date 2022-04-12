d7d54f74f23702c9a91ee3fabe5ba85d26154d05
KAWSAR <kawsarkabir11@gmail.comm>
2022-04-13 04:14:40 +0600

    All Chapter

---
+++ Chapter-02/Chapter-02.c
@@ -0,0 +1,264 @@
+// COMPUTER PROGRAMMING 1ST PAPER BY SUBEEN
+
+//***==========Chapter 2 PART 2.1 is here===========***
+
+// #include <stdio.h>
+
+// int main(){
+
+//     int a ;
+//     int b ;
+//     int sum ;
+
+//     a = 50 ;
+//     b = 50 ;
+//     sum = a + b ;
+
+//     printf("sum is %d", sum);
+
+//     return 0;
+// }
+
+
+//*******Chapter 2 PART 2.2 is here*******
+
+// #include <stdio.h>
+// int main(){
+
+//     int a, b, sum;
+//     a = 50;
+//     b = 60;
+//     sum = a + b;
+
+//     printf("Sum is %d", sum);
+
+//     return 0;
+// }
+
+
+//*******Chapter 2 PART 2.3 is here*******
+
+// #include <stdio.h>
+
+// int main(){
+
+//     int a = 50, b = 50, jokFol;
+//     jokFol = a + b ;
+
+//     printf("jokFol Hobe %d", jokFol);
+// }
+
+//=============Chapter 2 PART 2.4 is here===========
+
+// #include <stdio.h>
+
+// int main(){
+
+//     int x, y;
+//     x = 1;
+//     y = x;
+//     x = 2;
+
+//     printf("%d",y);
+
+//     return 0;
+// }
+
+//*******Chapter 2 PART 2.5 is here*******
+
+// #include <stdio.h>
+
+// int main(){
+
+//     int a = 50, b = 100, sum;
+//     sum = a + b ;
+
+//     printf("%d + %d = %d", a, b, sum);
+
+//     return 0;
+// }
+
+//========= Chapter 2 PART 2.5 age 0 pore ===========
+
+// #include <stdio.h>
+
+// int main(){
+
+//     int a = 50, b = 100, sum;
+//     sum = a + b ;
+
+//     printf("%d + %d = %d", b, a, sum);
+
+//     return 0;
+// }
+
+//*******Chapter 2 PART 2.6*******
+
+//  #include <stdio.h>
+
+//  int main(){
+
+//      int a = 50.90, b = 100, sum;
+//      sum = a + b;
+
+//      printf("%d + %d = %d", a, b, sum);
+
+//      return 0;
+//  }
+
+//******** Chapter 2 PART 2.6 without return 0;*********
+
+//  #include <stdio.h>
+
+//  int main(){
+
+//      int a = 50.90, b = 100, sum;
+//      sum = a + b;
+
+//      printf("%d + %d = %d", a, b, sum);
+
+      
+//   }
+
+//*******Chapter 2 PART 2.7*******
+
+// #include <stdio.h>
+
+// int main(){
+
+//     int n;
+//     double x;
+
+//     x = 10.5;
+//     n = (int)x;
+
+//     printf("Value of n is %d\n", n);
+//     printf("Value of x is %lf\n", x);
+    
+//     return 0;
+// }
+
+//*******Chapter 2 PART 2.8*******
+
+// #include <stdio.h>
+
+// int main(){
+
+//     int a;
+//     a = 1000;
+//     printf("Value Of a is %d\n", a);
+//     a = -21000;
+//     printf("Value of is %d\n", a);
+//     a = 10000000;
+//     printf("Value of a is %d\n", a);
+//     a = -10000000;
+//     printf("Value of a is %d\n", a);
+//     a = 100020004000503;
+//     printf("Value of a is %d\n", a);
+//     a = -4325987632;
+//     printf("Value of a is %d\n", a);
+
+//     return 0;
+
+
+// }
+
+//*******Chapter 2 PART 2.9*******
+
+// #include <stdio.h>
+
+// int main (){
+
+//     double a, b, sum;
+//     a = 10.40600;
+//     b = 9.604857;
+//     sum = a + b;
+
+//     printf("sum is %lf\n", sum);
+//     printf("sum is %0.0lf\n", sum);
+//     printf("sum is %0.2lf\n", sum);
+// }
+
+//*********Chapter 2 PART 2.10*********
+
+// #include <stdio.h>
+
+// int main(){
+
+//     int a, b, sum;
+//     scanf("%d", &a);
+//     scanf("%d", &b);
+//     sum = a + b;
+//     printf("Sum is: %d\n", sum);
+
+//     return 0;
+
+// }
+//*********Chapter 2 PART 2.10 without &*********
+
+// #include <stdio.h>
+
+// int main(){
+
+//     int a, b, sum;
+//     scanf("%d", &a);
+//     scanf("%d",b);
+//     sum = a + b;
+
+//     printf("sum is %d", sum);
+
+//     return 0;
+// }
+
+//*********Chapter 2 PART 2.11 বাস্তব সংখ্যার ক্ষেত্রে*********
+
+// #include <stdio.h>
+
+// int main (){
+
+//     double a, b, sum;
+//     scanf("%lf",&a);
+//     scanf("%lf",&b);
+//     sum = a + b;
+//     printf("sum is %.0lf", sum);
+
+//     return 0;
+// }
+
+//*********Chapter 2 PART-2.12*********
+
+// #include <stdio.h>
+
+// int main(){
+
+//     char ch;
+//     printf("Enter the first letter of your name: ");
+//     scanf("%c",&ch);
+//     printf("The first letter of your name is: %c\n", ch);
+
+//     return 0;
+
+
+// }
+
+//*********Chapter 2 PART-2.13 ANOTHER WAY*********
+
+
+#include <stdio.h>
+
+   int main(){
+
+       char ch;
+       ch = getchar();
+       printf("The first letter of your name is: %c\n", ch);
+     
+       return 0;
+
+}
+
+
+
+
+
+
+
